37a8a488fedda5b99280f89d437b8107c3e6833a
Madhu-Dev1 <madhusainadh.lalam@headtler.com>
2026-03-03 23:05:15 +0530

    feat: Add Apollo Client for GraphQL API integration with JWT authentication and update GraphQL backend URL to use port 8080.

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:8080/graphql',
 });
 
 // Attach JWT token as Authorization header on every request
